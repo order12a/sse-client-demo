6ca7d8dddd457811ff1cd3da0f62369d846a4f71
Volodymyr Uroshliev <volodymyr.uroshliev@eleks.com>
2018-02-05 16:24:49 +0200

    Added logger

--- src/main/java/com/sse/demo/SseClient.java
+++ src/main/java/com/sse/demo/SseClient.java
@@ -1,5 +1,6 @@
 package com.sse.demo;
 
+import org.apache.log4j.Logger;
 import org.glassfish.jersey.media.sse.EventInput;
 import org.glassfish.jersey.media.sse.InboundEvent;
 import org.glassfish.jersey.media.sse.SseFeature;
@@ -9,29 +10,35 @@ import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 
 public class SseClient {
+    final static Logger logger = Logger.getLogger(SseClient.class);
 
     public void sseRequest(final String url){
         EventInput eventInput = registerClient(url).request().get(EventInput.class);
+        logger.info("Connection established with: " + url);
         while (!eventInput.isClosed()) {
             final InboundEvent inboundEvent = eventInput.read();
             if (inboundEvent == null) {
                 // connection has been closed
+                logger.info("No new server events, closing connection.");
                 break;
             }
-            System.out.println(inboundEvent.getName() + "; " + inboundEvent.readData(String.class));
+            logger.info(inboundEvent.getName() + "; " + inboundEvent.readData(String.class));
         }
     }
 
     public String sseRequest(final String url, final String interruptMessage){
         EventInput eventInput = registerClient(url).request().get(EventInput.class);
+        logger.info("Connection established with: " + url);
         while (!eventInput.isClosed()) {
             final InboundEvent inboundEvent = eventInput.read();
             if (inboundEvent.readData(String.class).equals(interruptMessage)) {
                 // interrupt our connection on target message
+                logger.info("Breakpoint reached on message '" + interruptMessage + "'");
                 return inboundEvent.readData(String.class);
             }
-            System.out.println(inboundEvent.getName() + "; " + inboundEvent.readData(String.class));
+            logger.info(inboundEvent.getName() + "; " + inboundEvent.readData(String.class));
         }
+        logger.info("No target breakpoint found while receiving sse.");
         return null;
     }
 
--- src/test/java/com/sse/demo/TestConsumingSse.java
+++ src/test/java/com/sse/demo/TestConsumingSse.java
@@ -1,11 +1,12 @@
 package com.sse.demo;
 
+import org.apache.log4j.Logger;
+import org.junit.Assert;
 import org.junit.Test;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
 public class TestConsumingSse {
+
+    final static Logger logger = Logger.getLogger(TestConsumingSse.class);
 
     private static final String URL = "http://127.0.0.1:8081/sse";
     private static final String interruptMessage = "Possible break";
@@ -19,6 +20,6 @@ public class TestConsumingSse {
     @Test
     public void testSseExampleWithInterrup(){
         SseClient client = new SseClient();
-        client.sseRequest(URL, interruptMessage);
+        Assert.assertEquals(interruptMessage, client.sseRequest(URL, interruptMessage));
     }
 }
